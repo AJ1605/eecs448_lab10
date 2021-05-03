7dd2f816ef2f4df6daed504a4b03d5f463429c76
Azdeen Jeljalane <azdeenje@ku.edu>
2021-05-02 19:43:56 -0500

    EECS448_Lab10

---
+++ Exercise2_Lab10/CreateUser.php
@@ -0,0 +1,23 @@
+<?php
+    $mysqli = new mysqli("mysql.eecs.ku.edu", "azdeenjeljalane", "Xu9naiha", "azdeenjeljalane");
+    if ($mysqli->connect_error){
+        printf("Connection failed %s\n", $mysqli->connect_error);
+        exit();
+    }
+
+    $user = $_POST["user_id"];
+
+    if ($user == ""){
+        echo "User was not created because the field was left empty";
+        exit();
+    }
+
+    $query = "INSERT INTO Users (user_id) VALUES ('" . $user .  "')";
+    if ($result = $mysqli->query($query)){
+        echo "User created successfully";
+    }
+    else{
+        echo "User was not created because " . $user . " is already taken";
+    }
+    $mysqli->close();
+?>
